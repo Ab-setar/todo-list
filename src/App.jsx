c686a33d5e4fe65c78cc21f4f796a7383dee1849
Abdusetar <fati40458689@gmail.com>
2026-02-16 13:30:17 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+function App() {
+	return (
+		<>
+			<div>
+				<h1 className='text-10xl font-bold underline bg-blue-500 text-white p-4'>
+					you did it
+				</h1>
+			</div>
+		</>
+	);
+}
+
+export default App;
